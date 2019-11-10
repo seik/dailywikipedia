222ad54131dbb40909f144ea345ba39352b82c80
Iván José Martín García <ivanmartinga@gmail.com>
2019-11-10 19:19:33 +0100

    Update

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 import json
 import logging
 import os
-from typing import List
 
 import httpx
 import telegram
-from pynamodb.attributes import NumberAttribute, UnicodeAttribute
+from pynamodb.attributes import NumberAttribute, BooleanAttribute
 from pynamodb.models import Model
+from pynamodb.pagination import ResultIterator
 
 logger = logging.getLogger()
 if logger.handlers:
@@ -23,18 +23,16 @@ ERROR_RESPONSE = {"statusCode": 400, "body": json.dumps("Oops, something went wr
 
 BOT_USERMAME = os.environ.get("BOT_USERMAME")
 
-url = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
-
 
 class ChatModel(Model):
     id = NumberAttribute(hash_key=True)
-    hour = NumberAttribute()
+    hour = NumberAttribute(default=19)
+    daily = BooleanAttribute(default=True)
 
     class Meta:
-        table_name = "chats"
-        region = "eu-west-3"
-        aws_access_key_id = os.environ.get("AWS_ACCESS")
-        aws_secret_access_key = os.environ.get("AWS_SECRET")
+        table_name = os.environ['DYNAMODB_TABLE']
+        region = os.environ['REGION']
+        host = os.environ['DYNAMODB_HOST']
 
 
 def configure_telegram():
@@ -55,7 +53,6 @@ bot = configure_telegram()
 
 
 def handler(event, context) -> dict:
-
     logger.info(f"Event: {event}")
 
     update = telegram.Update.de_json(json.loads(event.get("body")), bot)
@@ -74,52 +71,77 @@ def handler(event, context) -> dict:
 
 
 def daily(event, context) -> dict:
-    article = get_random_article_url()
-    chats = get_chats()
+    article_url = get_random_article_url()
+    chats = get_daily_chats()
 
     for chat in chats:
-        bot.send_message(chat_id=chat.id, text=article)
+        bot.send_message(chat_id=chat.id, text=article_url)
 
     return OK_RESPONSE
 
 
 def start(chat_id: int) -> None:
+    save_chat_id(chat_id)
+
     text = (
         "I will send you a random article every day at 8PM, to stop "
         "this send /stop. If you want a random article now send "
         "/article"
     )
 
-    save_chat_id(chat_id)
+    bot.send_message(chat_id=chat_id, text=text)
+
+
+class AlreadyStoped(Exception):
+    pass
+
+
+def stop(chat_id: int) -> None:
+    try:
+        stop_chat_id(chat_id)
+    except AlreadyStoped:
+        text = "You are already unsubscribed"
+    else:
+        text = (
+            "I will no longer send you a daily article, to activate it again"
+            "send /start"
+        )
 
     bot.send_message(chat_id=chat_id, text=text)
 
 
-def stop(chat_id: int) -> None:
-    delete_chat_id(chat_id)
-    bot.send_message(chat_id=chat_id, text="Stopped!")
-
-
 def article(chat_id: int):
-    article = get_random_article_url()
-    bot.send_message(chat_id=chat_id, text=article)
+    article_url = get_random_article_url()
+    bot.send_message(chat_id=chat_id, text=article_url)
 
 
 def get_random_article_url() -> str:
-    response = httpx.get(url)
+    response = httpx.get("https://en.wikipedia.org/api/rest_v1/page/random/summary")
     return response.json()["content_urls"]["desktop"]["page"]
 
 
-def get_chats() -> List[ChatModel]:
-    return ChatModel.scan()
+def get_daily_chats() -> "ResultIterator[ChatModel]":
+    return ChatModel.scan(ChatModel.daily == True)
 
 
 def save_chat_id(chat_id: int) -> None:
-    if not ChatModel.count(chat_id):
-        ChatModel(chat_id, hour=19).save()
+    try:
+        chat = ChatModel.get(chat_id)
+    except ChatModel.DoesNotExist:
+        ChatModel(chat_id).save()
+    else:
+        chat.daily = True
+        chat.save()
 
 
-def delete_chat_id(chat_id: int) -> None:
-    chats = ChatModel.query(chat_id)
-    for chat in chats:
-        chat.delete()
+def stop_chat_id(chat_id: int) -> None:
+    try:
+        chat = ChatModel.get(chat_id)
+    except ChatModel.DoesNotExist:
+        pass
+    else:
+        if not chat.daily:
+            raise AlreadyStoped
+
+        chat.daily = False
+        chat.save()
